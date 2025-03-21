39ffbc6e66f451a75ddf373628bc9d7767dfae5e
Peter Tran <151450248+petertranvnn@users.noreply.github.com>
2025-03-21 11:28:04 +0700

    Add files via upload

---
+++ auto.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+echo -e '\e[34m'
+echo -e '$$\   $$\ $$$$$$$$\      $$$$$$$$\           $$\                                       $$\     '
+echo -e '$$$\  $$ |\__$$  __|     $$  _____|          $$ |                                      $$ |    '
+echo -e '$$$$\ $$ |   $$ |        $$ |      $$\   $$\ $$$$$$$\   $$$$$$\  $$\   $$\  $$$$$$$\ $$$$$$\   '
+echo -e '$$ $$\$$ |   $$ |$$$$$$\ $$$$$\    \$$\ $$  |$$  __$$\  \____$$\ $$ |  $$ |$$  _____|\_$$  _|  '
+echo -e '$$ \$$$$ |   $$ |\______|$$  __|    \$$$$  / $$ |  $$ | $$$$$$$ |$$ |  $$ |\$$$$$$\    $$ |    '
+echo -e '$$ |\$$$ |   $$ |        $$ |       $$  $$<  $$ |  $$ |$$  __$$ |$$ |  $$ | \____$$\   $$ |$$\ '
+echo -e '$$ | \$$ |   $$ |        $$$$$$$$\ $$  /\$$\ $$ |  $$ |\$$$$$$$ |\$$$$$$  |$$$$$$$  |  \$$$$  |'
+echo -e '\__|  \__|   \__|        \________|\__/  \__|\__|  \__| \_______| \______/ \_______/    \____/ '
+echo -e '\e[0m'
+echo -e "Join our Telegram channel: https://t.me/NTExhaust"
+sleep 5
+
+echo -e "🚀 Starting setup process..."
+rm -rf $HOME/light-node
+echo -e "🔗 Cloning repository..."
+git clone https://github.com/Layer-Edge/light-node.git && echo -e "✅ Repository cloned!"
+cd light-node
+echo -e "📥 Downloading and installing dependencies..."
+curl -L https://risczero.com/install | bash && echo -e "✅ Dependencies installed!"
+source "/root/.bashrc"
+echo -e "🔄 Applying environment variables..."
+export GRPC_URL=34.31.74.109:9090
+export CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
+export ZK_PROVER_URL=http://127.0.0.1:3001
+export API_REQUEST_TIMEOUT=100
+export POINTS_API=http://127.0.0.1:8080
+echo -e "🔑 Please enter your private key: "
+read PRIVATE_KEY
+echo -e "✅ Private key set!"
+export PRIVATE_KEY
+
+echo -e "🛠️ Building and running risc0-merkle-service..."
+cd risc0-merkle-service
+cargo build && screen -dmS risc0-service cargo run && echo -e "🚀 risc0-merkle-service is running in a screen session!"
+
+echo -e "🖥️ Starting light-node server in a screen session..."
+
+echo -e "🎉 Setup complete! Both servers are running independently in screen sessions!"
